1c5f3ac2e32f06e58f2fd1be5dcd4ae7f1374bee
WP2lukmanalfajri <102210654+WP2lukmanalfajri@users.noreply.github.com>
2022-04-15 20:22:35 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,26 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Lukman</li>
+        <li>Nama Belakang : Alfajri</li>
+        <li>NIM : 17190365</li>
+    </ul>
+
+    <h4> Alamat </h4>
+    <ul type="none">
+        <li> Gg.Madrasah 1 Rt026/Rw 003 </li> 
+        <li>Kel.Gandul, Kec. Cinere, Kab.JawaBarat</li>
+    </ul>
+
+    <h4> Tempat Lahir </h4>
+    <ul type="none">
+        <li>Brebes</li>
+    </ul>
+
+    <h4>Hobi</h4>
+    <ul type="square">
+        <li>Bermain Bulu tangkis</li>
+   
+    </ul>
+</section>
