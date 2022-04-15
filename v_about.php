db44a2e32f110a5eea464e3ca9076e20f13eb209
WP2lukmanalfajri <102210654+WP2lukmanalfajri@users.noreply.github.com>
2022-04-15 19:59:58 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,27 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Farrel</li>
+        <li>Nama Belakang : Abdullah</li>
+        <li>NIM : 17190310</li>
+    </ul>
+
+    <h4> Alamat </h4>
+    <ul type="none">
+        <li>Perumahan Bumi Indah Pesona RT01/013</li> 
+        <li>Kel. Kalisuren, Kec. Tajurhalang, Kab. Bogor, JawaBarat</li>
+    </ul>
+
+    <h4> Tempat Lahir </h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Hobi</h4>
+    <ul type="square">
+        <li>Bermain Basket</li>
+        <li>Bermain Game</li>
+        <li>Mendengarkan Musik</li>
+    </ul>
+</section>
